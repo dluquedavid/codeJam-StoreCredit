6ba997c13614c6fc431834c845ad66a4f74fca71
David Rodriguez Luque <dluque.david@gmail.com>
2015-12-06 01:28:36 +0000

    Initial Commit

---
+++ StoreCredit/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  StoreCredit
+//
+//  Created by David Rodriguez Luque on 6/12/15.
+//  Copyright © 2015 David Rodriguez Luque. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
